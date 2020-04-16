43bf900fc2c6c665c1a55095fc0ac8fc2725dd54
Ajay Chopra <ajaychopra156@gmail.com>
2020-04-16 14:23:07 +0530

    start11

---
+++ pythonbasics/swaping1.py
@@ -0,0 +1,39 @@
+# octacore Hexacore
+j=123
+print(oct(j))
+print(hex(j))
+
+# swaping two number using 3 variables
+a=10
+b=5
+print("Value of A before Swaping :",a)
+print("Value of B before Swaping :",b)
+c=a
+a=b
+b=c
+print("Value of A after Swaping :",a)
+print("Value of B after Swaping :",b)
+
+# Swapping two numbers using 2 variables
+# there are two methods of it
+# First method
+d=10
+e=5
+print("Value of D before Swaping :",d)
+print("Value of E before Swaping :",e)
+d=d+e
+e=d-e
+d=d-e
+print("Value of D after Swaping :",d)
+print("Value of E after Swaping :",e)
+
+#second Method
+f=10
+g=5
+print("Value of F before Swaping :",f)
+print("Value of G before Swaping :",g)
+f,g=g,f
+print("Value of F After Swaping :",f)
+print("Value of G After Swaping :",g)
+
+
